c26286193861d997dba6bb90e8926a8170a0dc94
Harpo <harpo@harpo.me>
2023-01-02 12:25:06 -0800

    Refactor get_message to return message directly

--- qlab.py
+++ qlab.py
@@ -15,28 +15,21 @@ class Listener:
         self.port = port
         self.sock = socket(AF_INET, SOCK_DGRAM)
         self.sock.bind((address, port))
-        self.last_message = None
 
-    def _get_message(self):
+    def get_message(self):
         data, address = self.sock.recvfrom(8192)
         raw = data.decode('utf8')
         parts = list(filter(bool, raw.split('\x00')))
         json_message = parts[2]
         try:
-            self.last_message = loads(json_message)
-            if self.last_message.get('data'):
-                print(self.last_message['data'])
+            message = loads(json_message)
+            if message.get('data'):
+                print(message['data'])
+            return message
         except decoder.JSONDecodeError as e:
             print('Error. server raw response:', repr(raw))
             print('parts', parts)
             print(e)
-            self.last_message = None
-
-    def get_message(self):
-        t = Thread(target=self._get_message, daemon=True)
-        t.start()
-        t.join(timeout=0.3)
-        return self.last_message
 
 
 class Client:
@@ -56,22 +49,10 @@ class Interface:
         self.server = Listener()
         self.client = Client()
 
-    def get_cue_text(self, cue_no):
-        return self.get_cue_property(cue_no, 'text')
-
-    def get_cue_property(self, cue_no, name):
-        self.client.send_message('/cue/{cue_no}/{name}'.format(**locals()))
+    def send_and_receive(self, *message):
+        self.client.send(*message)
         response = self.server.get_message()
-        if response:
-            return response.get('data')
-
-    def set_cue_property(self, cue_no, name, value):
-        self.client.send_message('/cue/{cue_no}/{name}'.format(**locals()), value=value)
-
-    def select_next_cue(self):
-        old = self.get_cue_property('selected', 'number')
-        self.client.send_message('/select/next')
-        cue_no = self.get_cue_property('selected', 'number')
-        while cue_no == old:
-            cue_no = self.get_cue_property('selected', 'number')
-        return cue_no
+        while not response or message[0] not in response['address']:
+            print('wrong response:', response)
+            response = self.server.get_message()
+        return response
