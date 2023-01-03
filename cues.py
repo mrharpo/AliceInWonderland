a19ceb670a69e4743bc0ccd93126aa2edbafd6dd
Harpo <harpo@harpo.me>
2023-01-02 17:47:14 -0800

    generate mute sheet as a cue list

--- cues.py
+++ cues.py
@@ -1,5 +1,4 @@
 from qlab import Interface
-from time import sleep
 
 from csv import reader
 from pydantic import BaseModel
@@ -9,18 +8,24 @@ class Cue(BaseModel):
     pass
 
 
-def get_cues():
-    with open('cues.csv') as f:
+def get_lighting_cues(file, page_offset=9):
+    with open(file) as f:
         cues = []
         r = reader(f)
         for n, row in enumerate(r):
             if n != 0 and row[2]:
                 print(row)
-                cues.append({'q': int(row[2]), 'name': row[3], 'page': int(row[0]) + 9})
+                cues.append(
+                    {
+                        'q': int(row[2]),
+                        'name': row[3],
+                        'page': int(row[0]) + page_offset,
+                    }
+                )
     return cues
 
 
-def cues_to_qlab(cues):
+def lighting_to_qlab(cues):
     i = Interface()
     for cue in cues:
         i.client.send(f'/new', 'network')
@@ -28,21 +33,6 @@ def cues_to_qlab(cues):
         i.client.send(f'/cue/selected/name', cue['name'])
         i.client.send(f'/cue/selected/customString', f'/eos/cue/{cue["q"]}/fire')
         i.client.send(f'/cue/selected/notes', f'p{cue["page"]}')
-        sleep(0.01)
-
-
-def moments_to_qlab():
-    i = Interface()
-    with open('moments.csv') as f:
-        moments = []
-        r = reader(f)
-        for row in r:
-            moments.append((int(row[0]), row[1]))
-    for moment in moments:
-        i.client.send('/new', 'group')
-        i.client.send('/cue/selected/number', f'm{moment[0]}')
-        i.client.send('/cue/selected/name', f'{moment[0]} - {moment[1]}')
-        sleep(0.01)
 
 
 def get_lines(file):
@@ -92,18 +82,26 @@ def lines_to_mutes(lines):
 
 def mute_sheet_for_character(lines, character):
     mute_state = None
-    mutes = [None] * len(lines)
+    mutes = []
     for l, line in enumerate(lines):
+        # if character has line and we are muted
         if line[0] == character and mute_state == None:
             # unmute
             mute_state = 0
+            mutes.append([l + 1, character, mute_state, lines[l - 1]])
+
+        # if we are unmuted and not speaking
         if mute_state == 0 and line[0] != character:
             # If more than 3 lines away, mute
             if character_next_speaks(lines[l + 1 : l + 5], character) == None:
                 # mute
                 mute_state = None
-        mutes[l] = mute_state
+                mutes.append([l + 1, character, mute_state, lines[l - 1]])
     return mutes
+
+
+def character_list(lines):
+    return set([line[0] for line in lines if line[0]])
 
 
 def character_next_speaks(lines, character):
@@ -120,3 +118,14 @@ def first_n(text, n=5):
 def last_n(text, n=5):
     words = text.split(' ')
     return ' '.join(words[-n:])
+
+
+if __name__ == '__main__':
+    from pprint import pp
+
+    lines = get_lines('penzance.csv')
+    mute_sheet = []
+    for character in character_list(lines):
+        mute_sheet += mute_sheet_for_character(lines, character)
+    mute_sheet = sorted(mute_sheet, key=lambda l: l[0])
+    pp(mute_sheet)
